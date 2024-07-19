4f1f245601b43bdf5bde5e60cf0615be26a8ba81
rthyjk <96871930+rthyjk@users.noreply.github.com>
2024-07-19 18:39:56 +0700

    Create skibidi.js

---
+++ skibidi.js
@@ -0,0 +1,3 @@
+function setFooter(){
+  document.querySelector('.copyright').innerHTML = '<div class="text-center"><a href="https://www.facebook.com/groups/340398917711992" target="_blank"><i class="fa-brands fa-facebook fa-2x"></i></a></div><center style="font-size:12px;">Teychit &copy; 2024 <a style="color:gray;text-decoration:none" href="https://www.boredbutton.com/" target="_blank"> Contact</a></center>'
+  }
